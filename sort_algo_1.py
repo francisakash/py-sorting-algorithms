6d325617db524ba21c7fafa049b82bb1672b4d05
francis akash <francisakashx@gmail.com>
2025-06-24 09:56:16 +0530

    Selection Sort updated

--- sort_algo_1.py
+++ sort_algo_1.py
@@ -2,11 +2,11 @@
 def selectionSort(arr):
 	n = len(arr)
 	for i in range(n):
-		min_val = i
-		for j in range(i, n):
-			if arr[j] < arr[min_val]:
-				min_val = j
-		arr[i], arr[min_val] = arr[min_val], arr[i]
+		min_index = i
+		for j in range(i+1, n):
+			if arr[j] < arr[min_index]:
+				min_index = j
+		arr[i], arr[min_val] = arr[min_index], arr[i]
 	print(arr)
 
 selectionSort([4,3,5,2,1])
