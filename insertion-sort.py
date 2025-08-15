ef686ddeefe625cf3a6440badcd613f001f94e9b
francis akash <francisakashx@gmail.com>
2025-08-15 23:42:20 +0530

    Insertion sort

---
+++ insertion-sort.py
@@ -0,0 +1,15 @@
+#Insertion sort
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+    return arr
+
+print(insertion_sort([5,3,8,4,2]))
